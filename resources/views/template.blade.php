50c3a5d415f585ff807c1108107e8b210d734135
Kaferas <kaferasmart95@gmail.com>
2024-02-29 14:10:37 +0200

    First Commit

---
+++ resources/views/template.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator</title>
+    <link rel="stylesheet" href="{{asset('assets/index.css')}}">
+    @livewireStyles
+</head>
+<body>
+    <div class="result2">
+        <span>MASTER JS</span>
+    </div>
+    <section>
+        The Uncoventional Calculator
+    </section>
+    <div class="calculator">
+    </div>
+
+    @livewireScripts
+</body>
+</html>
